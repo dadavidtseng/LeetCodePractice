cf09a82ab539d01f6494629cc1c0178cb353ed91
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-02-02 09:11:25 -0600

    Time: 41 ms (26.98%), Space: 14.1 MB (80.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_index_map;
-
-        for (int i = 0; i < static_cast<int>(nums.size()); ++i)
-        {
-            auto difference = target - nums[i];
-            
-            if (num_index_map.find(difference) != num_index_map.end())
-                return {num_index_map[difference], i};
-
-            num_index_map[nums[i]] = i;
+        for (int i = 0; i < (int)nums.size() - 1; i++) {
+            for (int j = i + 1; j < (int)nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
         }
-
-        throw invalid_argument("No two elements add up to the target.");
+        throw invalid_argument("target doesn't exist");
     }
 };
