b03d9410f7b1ba4150a92afcd5c70899e22f34a1
Yu-Wei Tseng <dadavidtseng@gmail.com>
2026-02-02 08:15:32 -0600

    del: clean up repo

--- 0035-search-insert-position/0035-search-insert-position.cs
+++
@@ -1,9 +0,0 @@
-public class Solution 
-{
-    public int SearchInsert(int[] nums, int target) 
-    {
-        var index = Array.BinarySearch(nums, target);
-
-        return index >= 0 ? index : ~index;
-    }
-}
