521dbc68369a65c7836997a8e784402b041fa518
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-02-02 11:08:46 -0600

    Time: 0 ms (100%), Space: 14.7 MB (52.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> numIndexMap;
+        unordered_map<int, int> numIndexMap;
 
         for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
             int difference = target - nums[i];
 
             if (numIndexMap.find(difference) != numIndexMap.end()) {
-                return std::vector<int>{numIndexMap[difference], i};
+                return {numIndexMap[difference], i};
             }
 
             numIndexMap[nums[i]] = i;
