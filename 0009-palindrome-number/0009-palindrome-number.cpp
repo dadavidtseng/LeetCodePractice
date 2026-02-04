2da66479860015e1416a4970fa093e69b442549e
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-02-04 11:44:30 -0600

    Time: 151 ms (5.31%), Space: 17.5 MB (5.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    static bool isPalindrome(int x)
+    {
+        if (x==0)return true;
+        if (x < 0) return false;
+        if (x % 10 == 0) return false;
+
+        unordered_map<int, int> m;
+        int                     mapIndex = 0;
+        int                     temp    = x;
+
+
+        while (temp > 0)
+        {
+            int digit = (temp % 10);
+            m.insert({mapIndex, digit});
+
+            temp /= 10;
+            mapIndex++;
+        }
+        for (auto it = m.begin(); it != m.end(); ++it)
+        {
+            printf("%d, %d\n", it->first, it->second);
+        }
+
+
+
+        for (int i = 0; i < (int)m.size(); i++)
+        {
+            int a      = m[i];
+            int b = m[(int)m.size() - i - 1];
+            if (a != b)
+            {
+                return false;
+            }
+        }
+
+
+
+        return true;
+    }
+};
