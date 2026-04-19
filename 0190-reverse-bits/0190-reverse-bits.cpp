2934a69d801dd75dd7991246485da105b9dc3b55
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-04-19 10:22:27 -0500

    Time: 4 ms (28.66%), Space: 8.3 MB (46.23%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1) {
+                result |= (1 << (31 - i));
+            }
+
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
