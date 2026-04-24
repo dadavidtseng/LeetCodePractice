c7ffc7db86542f3292b2fdd0eb344ffb0b41918b
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-04-24 14:57:49 -0500

    Time: 0 ms (100%), Space: 10.1 MB (68.89%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size(); // n = row size
-        vector<vector<int>> rotated(n, vector<int>(n));
+            int l = 0;
+        int r = matrix.size()-1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rotated[j][n - 1 - i] = matrix[i][j];
+        while(l<r)
+        {
+            for(int i= 0; i<r-l; i++)
+            {
+                int top = l;
+                int bottom = r;
+
+                int topLeft = matrix[top][l+i];             // store TL
+                matrix[top][l+i]=matrix[bottom-i][l];       // BL -> TL
+                matrix[bottom-i][l]=matrix[bottom][r-i];    // BR -> BL
+                matrix[bottom][r-i]=matrix[top+i][r];       // TR -> BR
+                matrix[top+i][r] = topLeft;                 // TL -> TR
             }
+            l++;
+            r--;
         }
-
-        matrix = rotated;
     }
 };
