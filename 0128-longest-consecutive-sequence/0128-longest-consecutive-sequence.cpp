8db77cbd685a59181f41734d4dcc759a8e5d4218
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-25 07:48:13 -0500

    Time: 79 ms (68.75%), Space: 89.1 MB (25.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,16 +4,16 @@ public:
         unordered_set const numSet(nums.begin(), nums.end());
         int longestLength = 0;
 
-        for (int num : numSet) {
-            int length = 0;
-
+        for (int const num : numSet) {
             if (!numSet.contains(num - 1)) {
+                int length = 0;
+
                 while (numSet.contains(num + length)) {
                     length++;
                 }
-            }
 
-            longestLength = std::max(length, longestLength);
+                longestLength = std::max(length, longestLength);
+            }
         }
 
         return longestLength;
