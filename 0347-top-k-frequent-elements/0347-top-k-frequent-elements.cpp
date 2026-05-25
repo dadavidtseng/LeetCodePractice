15661ae8d1e472335e90a7dbdfd950402d279261
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-24 20:45:25 -0500

    Time: 3 ms (50.7%), Space: 20.4 MB (10.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -20,6 +20,11 @@ public:
         vector<int> result;
 
         for (int i = static_cast<int>(buckets.size()) - 1; i > 0; --i) {
+            // result is full, break
+            if (std::cmp_equal(result.size(), k)) {
+                break;
+            }
+
             for (int j = static_cast<int>(buckets[i].size()) - 1; j >= 0; --j) {
                 // result is full, break
                 if (std::cmp_equal(result.size(), k)) {
