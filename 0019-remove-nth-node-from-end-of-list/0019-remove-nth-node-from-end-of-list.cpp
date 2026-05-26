1e863d0e4f72ad815dade05fd220e75810efa6b5
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-26 17:10:13 -0500

    Time: 0 ms (100%), Space: 14.8 MB (92.47%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // attach slow/fast pointer to head
+        // attach fast pointer to head
         ListNode const* fast = head;
 
         // attach a dummy node before head to handle the head removal case
@@ -23,19 +23,19 @@ public:
             fast = fast->next;
         }
 
-        // advance slow/fast/preSlow pointer
+        // advance fast/preSlow pointer
         // exit when reaching the last node(fast pointer's next is nullptr)
         while (fast->next != nullptr) {
             preSlow = preSlow->next;
             fast = fast->next;
         }
 
-        // remove the node slow pointer is at,
+        // remove the node preSlow's next pointer is at,
         // which is the nth node in from the end of the list
         preSlow->next = preSlow->next->next;
 
         // return dummy.next(head),
-        // in head removal case(single node), demmy.next is nullptr
+        // in head removal case(single node), dummy.next is nullptr
         return dummy.next;
     }
 };
