ce7ab5b2eaed284dee233a64929bdd73cde05130
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-29 13:44:48 -0500

    Time: 33 ms (96.44%), Space: 19.8 MB (45.98%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -21,39 +21,11 @@ public:
             node = node->children[idx];
         }
 
-        node->word = word;
-
         // we've finished inserting the nodes
         node->isEnd = true;
     }
 
-    bool search(string word) {
-        Trie const* node = traverse(word);
-
-        return node == nullptr ? false : node->isEnd;
-    }
-
-    Trie* traverse(string const& word) {
-        // current node in trie
-        Trie* node = this;
-
-        // iterate through the word string
-        for (int i = 0; i < static_cast<int>(word.length()); i++) {
-            int const idx = word[i] - 'a';
-
-            // if next node is empty
-            if (node->children[idx] == nullptr) {
-                return nullptr;
-            }
-
-            // advance to next node
-            node = node->children[idx];
-        }
-        return node;
-    }
-
     Trie* children[26] = {nullptr};
-    string word;
     bool isEnd = false;
 };
 
@@ -63,15 +35,23 @@ public:
     static vector<string> findWords(vector<vector<char>>& board,
                                     vector<string>& words) {
         vector<string> result;
+        string path; // a container that stores char for result
         Trie* trie = new Trie();
 
+        // insert all words into trie
         for (int i = 0; i < static_cast<int>(words.size()); ++i) {
             trie->insert(words[i]);
         }
 
+        // iterate through all cells in the board and start backtracking
         for (int i = 0; i < static_cast<int>(board.size()); ++i) {
             for (int j = 0; j < static_cast<int>(board[i].size()); ++j) {
-                backtrack(board, i, j, trie, result);
+                // return early if we've found everything
+                if (result.size() == words.size()) {
+                    return result;
+                }
+
+                backtrack(board, i, j, trie, path, result);
             }
         }
 
@@ -79,40 +59,74 @@ public:
     }
 
     static void backtrack(vector<vector<char>>& board, int const row,
-                          int const col, Trie* trie, vector<string>& result) {
-        // return true if we've found every character in the word
-        if (trie->isEnd) {
-            result.push_back(trie->word);
-            trie->isEnd = false;
-        }
-
-        // return false if current cell is out of bound
+                          int const col, Trie* trie, string& path,
+                          vector<string>& result) {
+        // return if current cell is out of bound
         // so that we don't go deeper in the wrong direction
         if (row < 0 || col < 0 || row >= static_cast<int>(board.size()) ||
             col >= static_cast<int>(board[0].size())) {
             return;
         }
 
-        if (board[row][col] == '#') {
+        // current char in the recursive call
+        char& c = board[row][col];
+
+        // return if current cell is '#'
+        if (c == '#') {
             return;
         }
 
+        // current index in the recursive call for checking the trie
+        int const idx = c - 'a';
+
         // if we've got a match, keep going deeper in recursive call
-        if (trie->children[board[row][col] - 'a']) {
+        if (trie->children[idx] != nullptr) {
+            Trie* child = trie->children[idx];
+            path.push_back(c);
+
+            // if we've found every character in the word
+            // push the word(found characters) into result
+            // and mark this trie node as false so that it can be deleted in
+            // next recursive call
+            if (child->isEnd) {
+                result.push_back(path);
+                child->isEnd = false;
+            }
+
             // temporarily marks the current cell's character before we go
             // deeper, so that we don't check the duplicated cell
-            Trie* child = trie->children[board[row][col] - 'a'];
-            char const temp = board[row][col];
-            board[row][col] = '#';
+            char const temp = c;
+            c = '#';
 
             // up, down, left, right
-            backtrack(board, row - 1, col, child, result);
-            backtrack(board, row + 1, col, child, result);
-            backtrack(board, row, col - 1, child, result);
-            backtrack(board, row, col + 1, child, result);
+            backtrack(board, row - 1, col, child, path, result);
+            backtrack(board, row + 1, col, child, path, result);
+            backtrack(board, row, col - 1, child, path, result);
+            backtrack(board, row, col + 1, child, path, result);
+
+            path.pop_back();
+
+            // prevents checking the same word in the trie
+            bool hasChildren = false;
+
+            for (int k = 0; k < 26; ++k) {
+                if (child->children[k] != nullptr) {
+                    hasChildren = true;
+                    break;
+                }
+            }
+
+            // if this trie node doesn't have children, and it's been found
+            // delete that node and set it to nullptr
+            // this is pruning the recursive call for finding duplicated words
+            // by deleting the trie node
+            if (!hasChildren && !child->isEnd) {
+                delete child;
+                trie->children[temp - 'a'] = nullptr;
+            }
 
             // restore the cell that we've marked
-            board[row][col] = temp;
+            c = temp;
         }
     }
 };
