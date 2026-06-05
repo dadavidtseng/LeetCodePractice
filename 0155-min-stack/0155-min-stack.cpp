1b95ca5700abeddb8aa3fb09166a3a96e938b69c
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-05 09:48:17 -0500

    Time: 41 ms (6.26%), Space: 152.4 MB (5.74%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,44 +1,29 @@
 class MinStack {
 public:
-    MinStack()
-    {
+    MinStack() {
         v = vector<int>{};
         cacheMin = vector<int>{};
     }
 
-    void push(int value)
-    {
-        if (cacheMin.empty())
-        {
-            cacheMin.push_back(value);
-        }
-        else if (value <= cacheMin[cacheMin.size() - 1])
-        {
+    void push(int value) {
+        if (cacheMin.empty() || value <= cacheMin[cacheMin.size() - 1]) {
             cacheMin.push_back(value);
         }
 
         v.push_back(value);
     }
 
-    void pop()
-    {
-        if (v.back() == cacheMin[cacheMin.size() - 1])
-        {
+    void pop() {
+        if (v.back() == cacheMin[cacheMin.size() - 1]) {
             cacheMin.pop_back();
         }
-        
+
         v.pop_back();
     }
 
-    int top()
-    {
-        return v[v.size() - 1];
-    }
+    int top() { return v[v.size() - 1]; }
 
-    int getMin()
-    {
-        return cacheMin[cacheMin.size() - 1];
-    }
+    int getMin() { return cacheMin[cacheMin.size() - 1]; }
 
     vector<int> v;
     vector<int> cacheMin;
