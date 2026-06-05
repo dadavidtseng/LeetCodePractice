94259c39f84bf1d3d4e2b7ed72b7e52a6fbf2c55
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-05 09:45:53 -0500

    Time: 48 ms (6.26%), Space: 152.4 MB (5.74%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,54 @@
+class MinStack {
+public:
+    MinStack()
+    {
+        v = vector<int>{};
+        cacheMin = vector<int>{};
+    }
+
+    void push(int value)
+    {
+        if (cacheMin.empty())
+        {
+            cacheMin.push_back(value);
+        }
+        else if (value <= cacheMin[cacheMin.size() - 1])
+        {
+            cacheMin.push_back(value);
+        }
+
+        v.push_back(value);
+    }
+
+    void pop()
+    {
+        if (v.back() == cacheMin[cacheMin.size() - 1])
+        {
+            cacheMin.pop_back();
+        }
+        
+        v.pop_back();
+    }
+
+    int top()
+    {
+        return v[v.size() - 1];
+    }
+
+    int getMin()
+    {
+        return cacheMin[cacheMin.size() - 1];
+    }
+
+    vector<int> v;
+    vector<int> cacheMin;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
