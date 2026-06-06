7e50c6e649d166d34ee23ed13a95b83d17568fec
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-06 10:16:11 -0500

    Time: 0 ms (100%), Space: 15.9 MB (93.58%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int result = stoi(tokens[0]);
+        int result = 0;
         int const tokensSize = static_cast<int>(tokens.size());
         stack<int> s;
-
-        if (tokensSize == 1) {
-            return result;
-        }
 
         for (int i = 0; i < tokensSize; ++i) {
             if (tokens[i] == "+") {
@@ -39,7 +35,9 @@ public:
                 result = (b / a);
                 s.push(result);
             } else {
-                s.push(stoi(tokens[i]));
+                int t = stoi(tokens[i]);
+                result = t;
+                s.push(t);
             }
         }
 
