7de3b2a6f0affa003233dcba0dda208e2e00243d
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-06 10:34:25 -0500

    Time: 8 ms (43.34%), Space: 3.9 MB (47.03%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -0,0 +1,19 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut s: HashSet<i32> = HashSet::new();
+
+        for i in 0..nums.len()
+        {
+            // bool value set to true if and only if the insertion took place
+            if !s.insert(nums[i])
+            {
+                return true;
+            }
+        }
+
+        // return false if there's no duplicated nums
+        false
+    }
+}
