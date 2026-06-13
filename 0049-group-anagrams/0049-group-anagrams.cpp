d531299b7b2dbe5b5221fcb034fed38f636a1b5b
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-12 19:16:15 -0500

    Time: 12 ms (83.4%), Space: 26.7 MB (26.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
+    //-----------------------------------------------------------------------------------------------
+    // Custom hash functor for array<int, 26>, enabling its use as an
+    // unordered_map key. Combines element hashes using the Boost hash_combine
+    // technique.
+    struct ArrayHash {
+        size_t operator()(array<int, 26> const& arr) const {
+            size_t seed = 0;
+
+            for (int val : arr) {
+                seed ^=
+                    hash<int>{}(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+
+            return seed;
+        }
+    };
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<array<int, 26>, vector<string>, ArrayHash> m;
 
-        struct ArrayHash {
-            size_t operator()(array<int, 26> const& arr) const {
-                size_t seed = 0;
-                for (int val : arr) {
-                    seed ^= hash<int>{}(val) + 0x9e3779b9 + (seed << 6) +
-                            (seed >> 2);
-                }
-                return seed;
-            }
-        };
+        // Iterate through strs
+        for (int i = 0; i < static_cast<int>(strs.size()); ++i) {
+            array<int, 26> a{};
 
-        unordered_map<array<int, 26>, vector<string>, ArrayHash> container;
-
-        for (int i = 0; i < (int)strs.size(); ++i) {
-            array<int, 26> counter{};
-
-            for (auto& ch : strs[i]) {
-                counter[ch - 'a']++;
+            // For every c in strs[i], use a to count the frequency of the c
+            for (const char& c : strs[i]) {
+                a[c - 'a']++;
             }
 
-            container[counter].push_back(strs[i]);
+            // At a(array<int, 26>), push strs[i] into the map
+            m[a].push_back(strs[i]);
         }
 
         vector<vector<string>> result;
 
-        for (auto& [key, group] : container) {
-            result.push_back(std::move(group));
+        for (unordered_map<array<int, 26>, vector<string>>::iterator it =
+                 m.begin();
+             it != m.end(); ++it) {
+            result.push_back(it->second);
         }
 
         return result;
