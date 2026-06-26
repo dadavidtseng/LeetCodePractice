5372006848d7c8af2394501fdd0c15424472695a
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-26 16:20:25 -0500

    Time: 0 ms (100%), Space: 20.4 MB (91.49%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,25 +5,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Base case of the recursive call, which means that we've reached the last node
+        # Return early if the root node doesn't exist
         if head is None:
             return None
 
-        new_head: Optional[ListNode] = head
+        # prev initially is None because we don't know root node(head)'s previous node
+        prev = None
+        curr = head
 
-        # Keep recursing as long as the next node exists
-        # 1. When hitting the base case at the last node:
-        #   - new_head stays as head (the new head of reversed list)
-        # 2. When starting to unwind:
-        #   - head is one node before the last node
-        #   - head.next is the last node
-        #   - "head.next.next = head": Reverse!
-        if head.next is not None:
-            new_head = self.reverseList(head.next)
-            head.next.next = head
-
-        # Make the original first node points to None
-        head.next = None
-
-        # Pass along the last node(new head) to first node(new tail)
-        return new_head
+        # Exist the loop when we've exhausted the linked list
+        # 1. Temporarily store the next node
+        # 2. Reverse the current node by pointing it to previous node
+        # 3. Advance previous node by assigning current node to it
+        # 4. Advance current node by assigning next node(temp) to it
+        # Note that we're not change the value because we are only "reversing" the linked list
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
