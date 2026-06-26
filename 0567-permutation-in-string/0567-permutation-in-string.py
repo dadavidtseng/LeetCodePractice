70c205816ec2a24ead82d07b6cbf170d62469ace
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-26 12:35:46 -0500

    Time: 15 ms (81.28%), Space: 19.4 MB (52.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,26 +6,26 @@ class Solution:
         if s1_size > s2_size:
             return False
 
-        freq = {}
+        freq = [0] * 26
 
         for c in s1:
-            freq[c] = freq.get(c, 0) + 1
+            freq[ord(c) - ord("a")] += 1
 
         matched = 0
-        needed = len(freq)
+        needed = sum(1 for f in freq if f > 0)
 
         for i, c in enumerate(s2):
-            if c in freq:
-                freq[c] -= 1
-                if freq[c] == 0:
-                    matched += 1
+            idx = ord(c) - ord("a")
+            freq[idx] -= 1
+            
+            if freq[idx] == 0:
+                matched += 1
 
             if i >= s1_size:
-                left = s2[i - s1_size]
-                if left in freq:
-                    if freq[left] == 0:
-                        matched -= 1
-                    freq[left] += 1
+                left_idx = ord(s2[i - s1_size]) - ord("a")
+                if freq[left_idx] == 0:
+                    matched -= 1
+                freq[left_idx] += 1
 
             if matched == needed:
                 return True
