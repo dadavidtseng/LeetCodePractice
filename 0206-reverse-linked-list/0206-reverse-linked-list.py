f69653db90e32e07f198de7a21583734141441a0
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-26 15:11:07 -0500

    Time: 0 ms (100%), Space: 20.5 MB (26.61%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,15 +5,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Return early if the root node doesn't exist
         if head is None:
             return None
 
-        new_head: Optional[ListNode] = head
+        # prev initially is None because we don't know root node(head)'s previous node
+        prev = None
+        curr = head
 
-        if head.next:
-            new_head = self.reverseList(head.next)
-            head.next.next = head
-
-        head.next = None
-
-        return new_head
+        # Exist the loop when we've exhausted the linkedlist
+        # 1. Temporaily store the next node
+        # 2. Reverse the current node by pointing it to previous node
+        # 3. Advance previous node by assigning current node to it
+        # 4. Advance current node by assigning next node(temp) to it
+        # Note that we're not change the value because we are only "reversing" the linkedlist
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
