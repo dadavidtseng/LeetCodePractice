18f93df6d14a6f0f51b249f59600db2a06c0dae0
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-26 16:18:56 -0500

    Time: 0 ms (100%), Space: 13.5 MB (41.14%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,19 +11,30 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-         if (head == nullptr) {
+        // Base case of the recursive call, which means that we've reached the
+        // last node
+        if (head == nullptr) {
             return nullptr;
         }
 
         ListNode* newHead = head;
 
+        // Keep recursing as long as the next node exists
+        // 1. When hitting the base case at the last node:
+        //      - new_head stays as head (the new head of reversed list)
+        // 2. When starting to unwind:
+        //      - head is one node before the last node
+        //      - head.next is the last node
+        //      - "head.next.next = head": Reverse!
         if (head->next != nullptr) {
             newHead = reverseList(head->next);
             head->next->next = head;
         }
 
+        // Make the original first node points to None
         head->next = nullptr;
 
+        // Pass along the last node(new head) to first node(new tail)
         return newHead;
     }
 };
