b864fa7c331c2f472d408118d7ec429d3257be65
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-06-28 19:46:33 -0500

    Time: 338 ms (1.09%), Space: 8.7 MB (98.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,15 +4,20 @@ public:
         while (true) {
             size_t pos;
 
+            // Remove the brackets if it's in the string
+
             if ((pos = s.find("()")) != string::npos ||
                 (pos = s.find("[]")) != string::npos ||
                 (pos = s.find("{}")) != string::npos) {
                 s.erase(pos, 2);
-                continue;
             }
-            break;
+            // Exit the loop when we couldn't find anything anymore
+            else {
+                break;
+            }
         }
 
+        // Return true if everything has been removed
         return s.empty();
     }
 };
